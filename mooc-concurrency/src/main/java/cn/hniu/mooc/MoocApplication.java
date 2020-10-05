7f446e3a23ac9912253bf3436e84ff72de9f4ac2
is-liuxi <is-liuxi@qq.com>
2020-10-06 00:25:48 +0800

    init

---
+++ mooc-concurrency/src/main/java/cn/hniu/mooc/MoocApplication.java
@@ -0,0 +1,21 @@
+package cn.hniu.mooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author: liuxi
+ * @date: 2020/10/6
+ */
+@SpringBootApplication
+public class MoocApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MoocApplication.class);
+    }
+}
